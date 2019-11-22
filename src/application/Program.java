5a1758889e10527255bc1c6ea0a06bdbc73097ab
Eduardoisaac23 <edugmendes.facisabh@gmail.com>
2019-11-22 20:05:59 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import mode.entities.Department;
+import mode.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "book");
-        System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Eduardo", "edug.mendes@yahoo.com.br",
+				new Date(), 3000.00, obj);
+		
+        System.out.println(seller);
 	}
 
 }

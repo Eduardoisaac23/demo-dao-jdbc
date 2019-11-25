4af7c8fb6e2bc5d13228ad2d80ff45cb62af2669
Eduardoisaac23 <edugmendes.facisabh@gmail.com>
2019-11-25 15:10:17 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,13 +10,11 @@ import model.dao.SellerDao;
 public class Program {
 
 	public static void main(String[] args) {
+
+		SellerDao sellerDao =  DaoFactory.createSellerDao(); 
 		
-		Department obj = new Department(1, "book");
+		Seller seller = sellerDao.findById(3);
 		
-		Seller seller = new Seller(21, "Eduardo", "edug.mendes@yahoo.com.br",
-				new Date(), 3000.00, obj);
-		
-		SellerDao sellerDao = new DaoFactory().createSellerDao(); 
         System.out.println(seller);
 	}
 
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaojDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaojDBC();
+		return new SellerDaojDBC(DB.getConnection());
 	}
 }
